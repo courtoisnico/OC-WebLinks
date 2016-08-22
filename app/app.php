d8a46fac62113b756a26727222745fd4aca7bd86
ncourtois <ncourtois@riu.fr>
2016-08-22 12:20:31 +0200

    Ajout log, barre debogage, gestion erreur

--- app/app.php
+++ app/app.php
@@ -28,6 +28,18 @@ $app->register(new \Silex\Provider\SecurityServiceProvider(),array(
         ),
     ),
 ));
+$app->register(new \Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../var/logs/microcms.log',
+    'monolog.name' => 'MicroCMS',
+    'monolog.level' => $app['monolog.level']
+));
+$app->register(new \Silex\Provider\ServiceControllerServiceProvider());
+if (isset($app['debug']) && $app['debug']) {
+    $app->register(new \Silex\Provider\HttpFragmentServiceProvider());
+    $app->register(new \Silex\Provider\WebProfilerServiceProvider(), array(
+        'profiler.cache_dir' => __DIR__.'/../var/cache/profiler'
+    ));
+}
 
 // Register services
 $app['dao.user'] = $app->share(function ($app){
@@ -38,4 +50,19 @@ $app['dao.link'] = $app->share(function ($app) {
     $linkDAO = new WebLinks\DAO\LinkDAO($app['db']);
     $linkDAO->setUserDAO($app['dao.user']);
     return $linkDAO;
+});
+
+// Register error handler
+$app->error(function (\Exception $e, $code) use ($app){
+    switch ($code) {
+        case 403:
+            $message = 'Access denied.';
+            break;
+        case 404:
+            $message = 'The requested resource could not be found.';
+            break;
+        default:
+            $message = 'Something went wrong.';
+    }
+    return $app['twig']->render('error.html.twig', array('message' => $message));
 });
